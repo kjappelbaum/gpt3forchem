3c9a1ef380156b224c4ff7ad218802c22a022d3f
Kevin Maik Jablonka <kevin.jablonka@epfl.ch>
2022-10-13 12:14:46 +0200

    feat: prepare initial analysis on USPTO yield data
    for some reason, we extracted now only a small dataset

--- gpt3forchem/data.py
+++ gpt3forchem/data.py
@@ -2,7 +2,7 @@
 
 # %% auto 0
 __all__ = ['POLYMER_FEATURES', 'gas_features', 'discretize', 'get_polymer_data', 'get_photoswitch_data', 'get_mof_data',
-           'preprocess_mof_data', 'get_core_mof_data', 'get_opv_data', 'get_mof_yield_data']
+           'preprocess_mof_data', 'get_core_mof_data', 'get_opv_data', 'get_mof_yield_data', 'get_uspto_yield_data']
 
 # %% ../notebooks/00_data.ipynb 2
 import os
@@ -217,5 +217,12 @@ def get_opv_data(datadir="../data"):  # path to folder with data files
 def get_mof_yield_data(datadir="../data"):  # path to folder with data files
     """Load the mof yield dataset."""
     df = pd.read_csv(os.path.join(datadir, "mof_yield_gpt3.csv"))
+    return HashableDataFrame(df)
+
+
+# %% ../notebooks/00_data.ipynb 59
+def get_uspto_yield_data(datadir="../data"):  # path to folder with data files
+    """Load the USPTO dataset."""
+    df = pd.read_csv(os.path.join(datadir, "USTPO_yields.csv"))
     return HashableDataFrame(df)
 
